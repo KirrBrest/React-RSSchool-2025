b6353b83ae92b5f1491121408ab6538dc692e614
KirrBrest <lukashchuk.kiryl@yandex.by>
2025-08-28 02:00:31 +0300

    fix: Start of project and fix unnecesaryes files and folders (Thu, Aug 28, 2025 02:00:31 AM)

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
-  },
 });
